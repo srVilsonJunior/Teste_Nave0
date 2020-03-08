e392cfd6f1cf32d5ea82b229f92a2eef38a251a4
Vilson Junior <vilsonjunior.sr@gmail.com>
2020-03-08 13:03:20 -0300

    Exercicio E2

---
+++ E2.js
@@ -0,0 +1,6 @@
+//E.2
+console.log('E.2 Crie uma função que recebe três argumentos, uma função e duas string, aplique a função nas duas string e imprima o resultado.');
+const tres = (funcao, p1, p2) => {
+    funcao(p1, p2);
+}
+tres(maior, 'Paulo', 'Anderson');
